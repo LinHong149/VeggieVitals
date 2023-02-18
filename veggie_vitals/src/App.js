59a9a5c63d891557b70ef08826216597c3dc4824
LinHong <linlin.hong1@gmail.com>
2023-02-18 11:00:22 -0500

    routing

--- veggie_vitals/src/App.js
+++ veggie_vitals/src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
 import Home from './pages/home'
 import Forum from './pages/forum'
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Home/>
-      {/* <Forum/> */}
-    </div>
+    <Router className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/forum" element={<Forum />} />
+      </Routes>
+  </Router>
   );
 }
 
