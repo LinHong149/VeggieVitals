f564dab9a7a772f1093d52d9dcd474dcecef2d0f
LinHong <linlin.hong1@gmail.com>
2023-02-18 02:41:21 -0500

    nav

--- veggie_vitals/src/components/nav/nav.elements.jsx
+++ veggie_vitals/src/components/nav/nav.elements.jsx
@@ -2,7 +2,32 @@ import styled from "styled-components";
 
 export const Nav = styled.div`
     height: 8vh;
-    border: 1px solid green;
+    // border: 1px solid green;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 60px;
+`
+
+export const Logo = styled.img`
+    // border: 1px solid green;
+    height: 8vh;
+`
+
+export const Language = styled.button`
+    border: none;
+    background: none;
+    font-size: 1rem;
+    height: 8vh;
+`
+
+export const Container = styled.div`
+    display: flex;
+    gap: 12px;
+`
+
+export const SocialsLink = styled.div`
+    font-size: 4vh;
     display: flex;
     justify-content: space-between;
     align-items: center;
--- veggie_vitals/src/components/nav/nav.jsx
+++ veggie_vitals/src/components/nav/nav.jsx
@@ -1,22 +1,24 @@
 import React from 'react'
+import VeggieVitals from "../../assets/VeggieVitals.png"
+
 import {
     Nav,
     Logo,
     Language,
-    Socials,
+    Container,
     SocialsLink,
 } from './nav.elements'
 
 const nav = () => {
   return (
     <Nav>
-        <img src="" alt="" />
-        <button>language</button>
-        <div>
-            <a href=""></a>
-            <a href=""></a>
-            <a href=""></a>
-        </div>
+        <Logo src={VeggieVitals} alt="" />
+        <Container>
+            <Language>language</Language>
+            <SocialsLink href="">ii</SocialsLink>
+            <SocialsLink href="">ii</SocialsLink>
+            <SocialsLink href="">ii</SocialsLink>
+        </Container>
     </Nav>
   )
 }
