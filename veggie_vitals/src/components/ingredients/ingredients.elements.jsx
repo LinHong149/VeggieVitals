3f1784d6f2d4155fcb4e164ce9a0384361245a09
Kvthleenhe <86500930+Kvthleenhe@users.noreply.github.com>
2023-02-18 03:45:54 -0500

    ingredients

---
+++ veggie_vitals/src/components/ingredients/ingredients.elements.jsx
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    height: 100vh;
+    width: 50vw;
+    border: 1px solid green;
+    justify-self: right;
+    display: flex;
+    flex-wrap: wrap;
+
+`
+
+export const Sub = styled.div`
+    border: 1px solid red;
+    padding: 20px;
+`
+
+
+export const Image = styled.img`
+    width: 150px;  
+    height: 150px;
+    // padding-left: 10px; 
+`
