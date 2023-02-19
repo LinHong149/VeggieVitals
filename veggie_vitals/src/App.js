80484acc43e3afeae5d0f85ccce441edc1a0c4d4
LinHong <linlin.hong1@gmail.com>
2023-02-19 01:52:01 -0500

    API

--- veggie_vitals/src/App.js
+++ veggie_vitals/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Home from './pages/home'
 import Forum from './pages/forum'
 import API from './pages/API'
+import Recipe from './pages/recipe'
 import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/forum" element={<Forum />} />
         <Route path="/api" element={<API />} />
+        <Route path="/recipe" element={<Recipe />} />
       </Routes>
   </Router>
   );
--- veggie_vitals/src/components/garden/garden.jsx
+++ veggie_vitals/src/components/garden/garden.jsx
@@ -19,7 +19,8 @@ export default class garden extends React.Component {
         super(props)
         this.state = {
             selected1: 1, // use the generate recipe button to set state
-            recipe: ''
+            recipe: '',
+            // PromiseResult: "\n\nBroccoli Salmon Bowl\nIngredients: \n- 2 heads of broccoli, cut into florets\n- 2 tablespoons extra-virgin olive oil\n- 1/2 teaspoon ground cumin\n- 1/2 teaspoon garlic powder\n- Sea salt, to taste\n- Freshly ground black pepper, to taste\n- 1 1/2 pounds salmon fillet, skinned and cut into 1-inch cubes\n- 1/4 cup freshly squeezed lemon juice\n- 2 tablespoons chopped fresh parsley\n\nInstructions: \n1. Preheat oven to 350°F (177°C).\n2. Place the broccoli on a parchment-lined baking sheet, drizzle with the oil, and sprinkle with the cumin, garlic powder, salt, and pepper. Toss to combine and spread out in an even layer.\n3. Bake for 15 minutes.\n4. Add the salmon cubes to the baking sheet and toss with the broccoli.\n5. Bake for an additional 15 minutes.\n6. Remove the baking sheet from the oven and drizzle with the lemon juice.\n7. Garnish with the parsley and serve hot. Enjoy!"
         }
     }
     
@@ -42,25 +43,28 @@ export default class garden extends React.Component {
     async fetchRecipe() {
         const requestOptions = {
             method: 'POST',
-            headers: { 'Content-Type': 'application/json', "Authorization": "Bearer tok_dev_HZanALUBPoA1HrToMNX1gUhgq1ZefRmYoS51wPRRxLKj2cc5yJAc3yVCc9GsHFAw"  },
+            headers: { 'Content-Type': 'application/json', "Authorization": "Bearer tok_dev_3BBqD5NQ38RWof44nt2SP9jj8gFKVA4K6T5FDaVG7HoaZdkNm97y28ZWvJMhMXGK"  },
             body: JSON.stringify({ 'name': 'world' })
         };
-        const response = await fetch('https://linhong.autocode.dev/veggie-vitals@dev/', requestOptions);
+        const response = await fetch('https://kvthleenhe.autocode.dev/veggievitals@dev/', requestOptions);
+// [[PromiseResult]]: "\n\nBroccoli Salmon Bowl\nIngredients: \n- 2 heads of broccoli, cut into florets\n- 2 tablespoons extra-virgin olive oil\n- 1/2 teaspoon ground cumin\n- 1/2 teaspoon garlic powder\n- Sea salt, to taste\n- Freshly ground black pepper, to taste\n- 1 1/2 pounds salmon fillet, skinned and cut into 1-inch cubes\n- 1/4 cup freshly squeezed lemon juice\n- 2 tablespoons chopped fresh parsley\n\nInstructions: \n1. Preheat oven to 350°F (177°C).\n2. Place the broccoli on a parchment-lined baking sheet, drizzle with the oil, and sprinkle with the cumin, garlic powder, salt, and pepper. Toss to combine and spread out in an even layer.\n3. Bake for 15 minutes.\n4. Add the salmon cubes to the baking sheet and toss with the broccoli.\n5. Bake for an additional 15 minutes.\n6. Remove the baking sheet from the oven and drizzle with the lemon juice.\n7. Garnish with the parsley and serve hot. Enjoy!"
+
+
             // .then((response) => response.json())
             // .then((data) => {
             //     return data
             // })
         const data = await response.json();
-        this.setState({ postId: data.id });
+        this.setState({ postId: data });
         return data
     }
 
-    generateRecipe() {
-        this.fetchRecipe()
-        // Get ingredients as input and set the ingredients to the state
-        // Call fetchRecipe()
-        // Set the returned recipe to the state so that it can be displayed in the HTML
-    }
+    // generateRecipe() {
+    //     this.fetchRecipe()
+    //     // Get ingredients as input and set the ingredients to the state
+    //     // Call fetchRecipe()
+    //     // Set the returned recipe to the state so that it can be displayed in the HTML
+    // }
 
     displayRecipe() {
         const a = this.fetchRecipe()
@@ -69,17 +73,21 @@ export default class garden extends React.Component {
     }
     
     render(){
-        let veggie1  =  this.changeMyVariable(this.state.selected1);
-        this.displayRecipe()
+        let veggie1 = this.changeMyVariable(this.state.selected1);
+        // this.state.recipe = this.displayRecipe()
+
+        // console.log(this.state.recipe)
 
         return (
             <Garden>
                 <Title>Your Garden</Title>
                 <ProduceList >
                     {veggie1}
-                    {this.state.recipe}
+                    {/* {response} */}
+                    {this.state.postId}
+                    {/* {this.state.recipe} */}
                 </ProduceList>
-                <button button onClick={this.generateRecipe}>Generate recipe</button> 
+                <button button onClick={ this.displayRecipe()}>Generate recipe</button> 
                 {/* Use set state to pass the ingredient to the state, so that you can pass to the API call */}
             </Garden>
         );
